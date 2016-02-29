d20c3466e88a3d9c42271ccee2c5aed8ead7a1eb
Scott Mehus <mehus.scott@gmail.com>
2016-02-28 21:29:17 -0500

    Setup table view to show items

--- Scottsy/ViewController.swift
+++ Scottsy/ViewController.swift
@@ -13,32 +13,49 @@ struct APIConstants {
     static let baseURL = "https://openapi.etsy.com/v2/listings/active?api_key=5809yztwpe426srusbm3340g"
 }
 
-class ViewController: UIViewController {
+internal final class ViewController: UIViewController {
 
-    @IBOutlet weak var serachField: UITextField!
+    @IBOutlet private weak var tableView: UITableView!
+    
+    private var dataCollection = [Item]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
         Alamofire.request(.GET, APIConstants.baseURL).responseJSON { response in
             if let data = response.data {
                 do {
                     let responseJson = try JSON(data: data)
                     let res = Response<[Item]>(json: responseJson)
+                    if let collection = res.data {
+                        self.dataCollection = collection
+                        self.tableView.reloadData()
+                    }
                 } catch {
                     print("FAILURE TO DESERIALIZE")
                 }
             }
         }
     }
+}
+
+extension ViewController: UITableViewDataSource {
     
-    private func searchWithText(text: String) {
-        
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
-
-
-    @IBAction func searchPressed(sender: AnyObject) {
-        
-        
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataCollection.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        let item = dataCollection[indexPath.row]
+        cell.textLabel!.text = item.title
+        return cell
     }
 }
 
