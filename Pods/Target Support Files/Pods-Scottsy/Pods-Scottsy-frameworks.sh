9a33216c04b7ae55471f65c7debfc97709993c3e
Scott Mehus <mehus.scott@gmail.com>
2016-02-28 21:43:25 -0500

    Setup reactive

--- Pods/Target Support Files/Pods-Scottsy/Pods-Scottsy-frameworks.sh
+++ Pods/Target Support Files/Pods-Scottsy/Pods-Scottsy-frameworks.sh
@@ -85,7 +85,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-Scottsy/Alamofire.framework"
+  install_framework "Pods-Scottsy/ReactiveCocoa.framework"
+  install_framework "Pods-Scottsy/Result.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-Scottsy/Alamofire.framework"
+  install_framework "Pods-Scottsy/ReactiveCocoa.framework"
+  install_framework "Pods-Scottsy/Result.framework"
 fi
